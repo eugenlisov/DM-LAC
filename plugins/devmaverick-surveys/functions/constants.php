f2cc2a069232309a85648b47a3b0a901f1a91141
eugenlisov <eugen.lisov@gmail.com>
2017-04-25 20:36:59 +0300

    Added more dynamic constants
    All are serialized, to work with PHP 5.6. In php 7 merg si fara.

--- plugins/devmaverick-surveys/functions/constants.php
+++ plugins/devmaverick-surveys/functions/constants.php
@@ -1,18 +1,39 @@
 <?php
 
-// Core pages
-define( 'PAGE_MY_COLLEGES', 4199 );
-define( 'PAGE_MY_ACCOUNT', 4196 );
-define( 'PAGE_PREMIUM_HOME', 4187 );
-define( 'PAGE_GET_PREMIUM', 4205 );
-// define( 'PAGE_PREMIUM_HOME', 4187 );
-// define( 'PAGE_PREMIUM_HOME', 4187 );
-// define( 'PAGE_PREMIUM_HOME', 4187 );
-// define( 'PAGE_PREMIUM_HOME', 4187 );
+add_action('init', 'dm_partner_define_constants');
 
-// Misc pages
-define( 'LOG_IN_PAGE', 4190 );
-define( 'LOG_OUT_PAGE', 4191 );
-define( 'CONTACT_PAGE', 186 );
+function dm_partner_define_constants(){
+
+  // Core pages
+  define( 'PAGE_MY_COLLEGES', 4199 );
+  define( 'PAGE_MY_ACCOUNT', 4196 );
+  define( 'PAGE_PREMIUM_HOME', 4187 );
+  define( 'PAGE_GET_PREMIUM', 4205 );
+  // define( 'PAGE_PREMIUM_HOME', 4187 );
+  // define( 'PAGE_PREMIUM_HOME', 4187 );
+  // define( 'PAGE_PREMIUM_HOME', 4187 );
+  // define( 'PAGE_PREMIUM_HOME', 4187 );
+
+  // Misc pages
+  define( 'LOG_IN_PAGE', 4190 );
+  define( 'LOG_OUT_PAGE', 4191 );
+  define( 'CONTACT_PAGE', 186 );
+
+
+  // Dynamic data
+
+  if ( is_user_logged_in() ) {
+    $user_id = get_current_user_id();
+    $current_user_notes 				= get_user_meta( $user_id, 'dm_user_notes', true );
+    $current_user_school_ratings 	= get_user_meta( $user_id, 'dm_user_ratings', true );
+    $favorites = get_user_meta($user_id, 'simplefavorites');
+    $favorites = $favorites[0][0]['posts'];
+
+    define( 'CURRENT_USER_SAVED_NOTES', serialize( $current_user_notes ) );
+    define( 'CURRENT_USER_SCHOOL_RATINGS', serialize( $current_user_school_ratings ) );
+    define( 'CURRENT_USER_FAVORITE_SCHOOLS', serialize( $favorites ) );
+  }
+}
+
 
  ?>
