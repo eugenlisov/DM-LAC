ca159e33ea552d7c998967f0752368a1225fbefd
eugenlisov <eugen.lisov@gmail.com>
2017-04-25 20:37:47 +0300

    Created methods for My Colleges page and widget

--- plugins/devmaverick-surveys/functions/register-shortcodes.php
+++ plugins/devmaverick-surveys/functions/register-shortcodes.php
@@ -11,7 +11,7 @@ function register_shortcodes(){
 	add_shortcode('dm-school-tabs', 'dm_school_tabs');
 	add_shortcode('dm-my-colleges', 'dm_my_colleges');
 	add_shortcode('dm-dashboard-links', 'dm_dashboard_links');
-	add_shortcode('dm-dashboard-my-colleges', 'dm_dashboard_my_colleges');
+	add_shortcode('dm-dashboard-my-colleges', 'dm_dashboard_my_colleges_widget');
 
 }
 
@@ -22,7 +22,7 @@ function dm_my_colleges() {
 	$return = $dm_my_colleges -> my_colleges_list();
 
 	return $return;
-	
+
 }
 
 
@@ -575,69 +575,14 @@ function dm_school_tabs() {
 
 
 /**
- * TODO Possible deprecated
- * @return [type] [description]
+ *
  */
-function dm_dashboard_my_colleges( $limit = 999 ) {
+function dm_dashboard_my_colleges_widget( $limit = 999 ) {
 
+	$dm_my_colleges = new DM_MyColleges;
+	$return = $dm_my_colleges -> my_colleges_widget();
 
-
-	$user_id = get_current_user_id();
-	$favorites = get_user_meta($user_id, 'simplefavorites');
-	$favorites = $favorites[0][0]['posts'];
-	$school_ratings 	= get_user_meta( $user_id, 'dm_user_ratings', true );
-
-
-
-	// echo '<pre>';
-	// print_r($favorites);
-	// echo '</pre>';
-
-	$return_string = '<div class="dm-my-collges-widget">';
-
-	$favorites_count = count($favorites);
-
-
-
-	if ($favorites_count > 0) {
-
-		$return_string .= '<h3>My Colleges</h3>';
-		$count = 1;
-		foreach ($favorites as $key => $college_id) {
-
-			$college_name 			= get_the_title( $college_id );
-			$college_permalink 	= get_the_permalink( $college_id );
-
-
-			// $thumb = get_the_post_thumbnail( $college_id, 'thumbnail' );
-			//
-			// echo '<pre>';
-			// print_r($thumb);
-			// echo '</pre>';
-
-
-			$return_string .= '<div class="college-item">';
-
-			$return_string .= '<a href=" ' . $college_permalink . '">' . $college_name . '</a><br />';
-
-			$rating = $school_ratings[$college_id];
-			if ($rating) {
-				$return_string .= 'Your rating:';
-				$return_string .= '<span class="dm-rating">';
-				for ($i=1; $i <= $rating; $i++) {
-					$return_string .= '<i class="fa fa-star" aria-hidden="true" rating="' . $i . '"></i>';
-				}
-				$return_string .= '</span>';
-			}
-
-			$return_string .= '</div>';
-
-		}
-	}
-
-	$return_string .= '</div>';
-
-	return $return_string;
+	return $return;
 
 }
 
@@ -650,11 +595,55 @@ function dm_dashboard_my_colleges( $limit = 999 ) {
 
 
 function dm_dashboard_links() {
+
+	$my_colleges_count = count( unserialize( CURRENT_USER_FAVORITE_SCHOOLS ) );
 	$return_string =  '<div class="dm-dashboard-links">';
 
-	$return_string .= '<a href="' . get_permalink( PAGE_MY_ACCOUNT ) . '" class="dm-dashboard-button col-md-3">My Account <i class="fa fa-user" aria-hidden="true"></i></a>';
-	$return_string .= '<a href="' . get_permalink( PAGE_MY_COLLEGES ) . '" class="dm-dashboard-button col-md-3">My Colleges <i class="fa fa-university" aria-hidden="true"></i></a>';
-	$return_string .= '<a href="' . get_permalink( CONTACT_PAGE ) . '" class="dm-dashboard-button col-md-3">Contact <i class="fa fa-envelope" aria-hidden="true"></i></a>';
+	$return_string .= '<div class="row">
+            <a href="' . get_permalink( PAGE_MY_ACCOUNT ) . '" class="col-lg-4">
+                <div class="widget style1 navy-bg">
+                    <div class="row">
+                        <div class="col-xs-4">
+                            <i class="fa fa-user fa-5x"></i>
+                        </div>
+                        <div class="col-xs-8 text-right">
+                            <span> My Account </span>
+
+                        </div>
+                    </div>
+                </div>
+            </a>
+            <a href="' . get_permalink( PAGE_MY_COLLEGES ) . '" class="col-lg-4">
+                <div class="widget style1 lazur-bg">
+                    <div class="row">
+                        <div class="col-xs-4">
+                            <i class="fa fa-university fa-5x"></i>
+                        </div>
+                        <div class="col-xs-8 text-right">
+                            <span> My Colleges </span>
+                            <h2 class="font-bold">' . $my_colleges_count . '</h2>
+                        </div>
+                    </div>
+                </div>
+            </a>
+            <a href="' . get_permalink( CONTACT_PAGE ) . '" class="col-lg-4">
+                <div class="widget style1 yellow-bg">
+                    <div class="row">
+                        <div class="col-xs-4">
+                            <i class="fa fa-envelope-o fa-5x"></i>
+                        </div>
+                        <div class="col-xs-8 text-right">
+                            <span> Contact </span>
+
+                        </div>
+                    </div>
+                </div>
+            </a>
+        </div>';
+
+	// $return_string .= '<a href="' . get_permalink( PAGE_MY_ACCOUNT ) . '" class="dm-dashboard-button col-md-3">My Account <i class="fa fa-user" aria-hidden="true"></i></a>';
+	// $return_string .= '<a href="' . get_permalink( PAGE_MY_COLLEGES ) . '" class="dm-dashboard-button col-md-3">My Colleges <i class="fa fa-university" aria-hidden="true"></i></a>';
+	// $return_string .= '<a href="' . get_permalink( CONTACT_PAGE ) . '" class="dm-dashboard-button col-md-3">Contact <i class="fa fa-envelope" aria-hidden="true"></i></a>';
 
 
 	$return_string .=  '</div>';
