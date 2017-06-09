7280213fcaa38750cc61d49cd87038017df42606
eugenlisov <eugen.lisov@gmail.com>
2017-06-09 17:02:53 +0300

    VArious Google Doc Changes
    CSS adjustments

--- plugins/devmaverick-surveys/assets/js/front/dm-landing-page.js
+++ plugins/devmaverick-surveys/assets/js/front/dm-landing-page.js
@@ -1,8 +1,15 @@
 $( document ).ready(function($) {
 
-  $( ".landing-benefits-sidebar button" ).click(function() {
-    $('.dm-checkout-container').slideDown();
-    $( this ).hide();
+  $( "a:contains('Get Premium Now')" ).addClass( ".trigger-show-checkout-form" );
+
+  $( document ).on( "click", ".trigger-show-checkout-form", function( e ) {
+    e.preventDefault();
+    $( '.dm-checkout-container' ).slideDown();
+
+    $('html, body').animate({
+        scrollTop: $( '.dm-checkout-container' ).offset().top
+    }, 2000);
+
   });
 
 });
